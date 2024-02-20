026c5e6aed8b5b863a663d544a6eeeb668e02c0b
jeungHunLee <ksjs1111@gmail.com>
2024-02-20 15:04:33 +0900

    add detailed reminder

--- Today/AppDelegate.swift
+++ Today/AppDelegate.swift
@@ -14,6 +14,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        UINavigationBar.appearance().tintColor = .todayPrimaryTint
+        UINavigationBar.appearance().backgroundColor = .todayNavigationBackground
+        let navigationBarAppearance = UINavigationBarAppearance()
+        navigationBarAppearance.configureWithOpaqueBackground()
         return true
     }
 
